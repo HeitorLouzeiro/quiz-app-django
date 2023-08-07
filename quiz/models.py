2cee5ea157b70e6525be7cc6245f9957e26022b8
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2023-08-07 16:38:21 -0300

    Created project models.

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,43 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(primary_key=True, editable=False,
+                            unique=True, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # This is an abstract class, so it won't be created in the database.
+        abstract = True
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(BaseModel):
+    question = models.CharField(max_length=100)
+    marks = models.IntegerField(default=5)
+    category = models.ForeignKey(
+        Category, related_name='category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.question
+
+
+class Answer(BaseModel):
+    answer = models.CharField(max_length=100)
+    question = models.ForeignKey(
+        Question, related_name='questions', on_delete=models.CASCADE)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.answer
